4bf8e972f30221d5c0cf404a4328e993f4acab1f
Santiflores06 <santiagoflores028@gmail.com>
2025-04-10 10:10:37 -0300

    first commit

---
+++ src/main/java/ubp/tc/practicofinal/trabajo/Trabajo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ubp.tc.practicofinal.trabajo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Trabajo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
